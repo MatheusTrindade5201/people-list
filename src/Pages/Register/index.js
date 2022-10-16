a1285e44cf204f7431bf217f3ab6b21cafeda618
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-10-16 09:51:25 -0300

    Feat: Funcionalidade de se cadastrar na aplicação

--- src/Pages/Register/index.js
+++ src/Pages/Register/index.js
@@ -1,7 +1,56 @@
+import { useState } from 'react'
+import { auth } from '../../Auth/AuthConfig';
 import Form from '../../Components/Form'
 import './Register.css'
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { NavLink } from 'react-router-dom';
 
 const Register = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPasswor] = useState('');
+
+    const [
+    createUserWithEmailAndPassword,
+    user,
+    loading,
+    error,
+    ] = useCreateUserWithEmailAndPassword(auth);
+
+    const signUp = () => {
+        createUserWithEmailAndPassword(email, password)
+    }
+
+    if (error) {
+        return (
+            <div className='sign'>
+            <Form 
+            form_type={'Sign Up'}
+            action={'Sign Up'}
+            route={'/'}
+            route_text={'I already have an account'}
+            typingEmail={value => setEmail(value)}
+            typingPassword={value => setPasswor(value)}
+            button_function={signUp}
+            error={<p className='form__error'>{error.message}</p>}
+            />
+        </div>
+        );
+      }
+      if (loading) {
+        return <p>Loading...</p>;
+      }
+      if (user) {
+        return (
+            <div className='sign'>
+                <div className='user__message'>
+                <h1 className='user__message-text'>User registered with success!</h1>
+                <NavLink className='return__button' to={'/'}>Sing In</NavLink>
+                </div>
+             </div>
+        );
+      }
+
     return (
         <div className='sign'>
             <Form 
@@ -9,6 +58,9 @@ const Register = () => {
             action={'Sign Up'}
             route={'/'}
             route_text={'I already have an account'}
+            typingEmail={value => setEmail(value)}
+            typingPassword={value => setPasswor(value)}
+            button_function={signUp}
             />
         </div>
     )
