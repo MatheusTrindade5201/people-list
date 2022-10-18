e0799d6267dbf6035249c93870666e7921c79559
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-10-17 23:09:32 -0300

    feat: Lógica de inserir e deletar pessoa

--- src/Pages/Edit/index.js
+++ src/Pages/Edit/index.js
@@ -14,12 +14,45 @@ const Edit = () => {
         setIsLogged(sessionStorage.getItem('@AuthFirebase:user'))
     }
 
+    const deletePeople = async () =>{
+        await fetch (`https://63471b7bdb76843976a667ae.mockapi.io/peoples/${id}`, {
+            method:'DELETE'
+        })
+        .then(
+            setDeleted(true)
+        )
+    }
+
+    const editPeople = async () => {
+        const data = {
+            name: name,
+            email: email,
+            birthDate: birth
+        }
+        await fetch (`https://63471b7bdb76843976a667ae.mockapi.io/peoples/${id}`, {
+            method:'PUT',
+            headers: {
+                'Content-type': 'application/json'
+              },
+            body: JSON.stringify({
+                name: name,
+                email: email,
+                birthDate: birth
+            })
+        })
+        .then(
+            setEdited(true)
+        )
+    }
+
     const [loading, setLoading] = useState(true)
     const [people, setPeople] = useState('')
     const [index, setIndex] = useState('')
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [birth, setBirth] = useState('')
+    const [deleted, setDeleted] = useState(false)
+    const [edited, setEdited] = useState(false)
 
     const getPeople = async () => {
         try {
@@ -51,40 +84,64 @@ const Edit = () => {
                 <p>Loading...</p>
             </div>
         }
-        return (
-            <div>
-                <Header 
-                function={singOut}
-                />
-                <NavLink className='return__button' to={'/Home'}>Return</NavLink>
-               <div className='page'>
-                    <div className='page__infos'>
-                        
-                        <TextField 
-                        label={'Id:'}
-                        readOnly={true}
-                        value={index}
-                        />
-                        <TextField 
-                        label={'Name:'}
-                        value={name}
-                        />
-                        <TextField 
-                        label={'Email:'}
-                        value={email}
-                        />
-                        <TextField 
-                        label={'Birth date:'}
-                        value={birth}
-                        />
-                    </div>
-                    <div className='options'>
-                        <button className='action__button'>Edit</button>
-                        <button className='action__button delete'>Delete</button>
+        if(deleted){
+            return(
+                <div className='sign'>
+                    <div className='user__message'>
+                    <h1 className='user__message-text'>Deleted with success</h1>
+                    <NavLink className='return__button' to={'/Home'}>Home</NavLink>
                     </div>
                 </div>
-            </div>
-        )
+                )
+        }else if(edited){
+            return(
+                <div className='sign'>
+                    <div className='user__message'>
+                    <h1 className='user__message-text'>Edited with success</h1>
+                    <NavLink className='return__button' to={'/Home'}>Home</NavLink>
+                    </div>
+                </div>
+                )
+        }
+        else{
+            return (
+                <div>
+                    <Header 
+                    function={singOut}
+                    />
+                    <NavLink className='return__button' to={'/Home'}>Return</NavLink>
+                <div className='page'>
+                        <div className='page__infos'>
+                            
+                            <TextField 
+                            label={'Id:'}
+                            readOnly={true}
+                            value={index}
+                            />
+                            <TextField 
+                            label={'Name:'}
+                            value={name}
+                            onTyped={value => setName(value)}
+                            />
+                            <TextField 
+                            label={'Email:'}
+                            value={email}
+                            onTyped={value => setEmail(value)}
+                            />
+                            <TextField 
+                            label={'Birth date:'}
+                            value={birth}
+                            onTyped={value => setBirth(value)}
+                            />
+                        </div>
+                        <div className='options'>
+                            <button onClick={editPeople} className='action__button'>Edit</button>
+                            <button onClick={deletePeople} className='action__button delete'>Delete</button>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
     }else {
         return(
         <div className='sign'>
